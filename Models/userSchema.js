c806d3e4a16782a6a8a64f03b860e8141db00546
Maryam Hamad <maryamhsk@gmail.com>
2026-01-16 12:39:16 +0300

    Update userSchema.js

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true  },
   password :{ type:String },
   role :{ type : String ,
-     enum: ['aser' , 'admin' ],
+     enum: ['user' , 'admin' ],
       default: 'user'
     }
 
